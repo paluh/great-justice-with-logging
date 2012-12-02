5f52cd8e2e94b1584c0e1a6398a678aa84e0d6ca
Tomasz Rybarczyk <paluho@gmail.com>
2012-12-02 01:53:13 +0100

    Cleanup

--- great_justice/logging.py
+++ great_justice/logging.py
@@ -22,7 +22,7 @@ class Formatter(Formatter):
         return s
 
 
-class SMTPHandler(SMTPHandler):
+class HtmlFormatter(Formatter):
 
     styles = {
         structure.WhatHappen: 'color:red',
@@ -36,44 +36,32 @@ class SMTPHandler(SMTPHandler):
         structure.ExceptionValue: 'font-weight:bold;color:red',
     }
 
-    def __init__(self, *args, **kwargs):
-        super(SMTPHandler, self).__init__(*args, **kwargs)
-        self.formatter = Formatter()
+    def format(self, record, html=False):
+        record.message = record.getMessage()
+        if self.usesTime():
+            record.asctime = self.formatTime(record, self.datefmt)
+        s = self._fmt % record.__dict__
+        if record.exc_info:
+            if html:
+                s = '<p style="white-space: pre-wrap; word-wrap: break-word;">%s</p>' % s
+                exc_html = self.formatException(record.exc_info, html)
+                s = s + exc_html
+            else:
+                if not record.exc_text:
+                    record.exc_text = self.formatException(record.exc_info)
+                if s[-1:] != "\n":
+                    s = s + "\n"
+                try:
+                    s = s + record.exc_text
+                except UnicodeError:
+                    s = s + record.exc_text.decode(sys.getfilesystemencoding(),
+                                                   'replace')
+        return s
 
-    def emit(self, record):
-        try:
-            msg = MIMEMultipart('alternative')
-            msg['Subject'] = self.getSubject(record)
-            msg['From'] = self.fromaddr
-            msg['To'] = ",".join(self.toaddrs)
-            msg['Date'] = formatdate()
-
-            text = self.format(record)
-            msg.attach(MIMEText(text.encode(sys.getfilesystemencoding()), 'plain'))
-            if record.exc_info:
-                info = (self.formatter or _defaultFormatter)._fmt % record.__dict__
-                info = '<p style="white-space: pre-wrap; word-wrap: break-word;">%s</p>' % info
-                trace_html = self._trace_html(utils.Trace(record.exc_info))
-                html = ('<html><head></head><body>%s<div style="font-size:120%%">%s</div></body></html>')% (info, trace_html)
-                msg.attach(MIMEText(html.encode(sys.getfilesystemencoding()), 'html'))
-            port = self.mailport
-            if not port:
-                port = smtplib.SMTP_PORT
-            smtp = smtplib.SMTP(self.mailhost, port)
-            if self.username:
-                if self.secure is not None:
-                    smtp.ehlo()
-                    smtp.starttls(*self.secure)
-                    smtp.ehlo()
-                smtp.login(self.username, self.password)
-            smtp.sendmail(self.fromaddr, self.toaddrs, msg.as_string())
-            smtp.quit()
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            self.handleError(record)
-
-    def _trace_html(self, trace):
+    def formatException(self, ei, html=False):
+        if not html:
+            return super(HtmlFormatter, self).formatException(ei)
+        trace = utils.Trace(ei)
         escape = lambda s: (s.replace('&', '&amp;')
                              .replace('>', '&gt;')
                              .replace('<', '&lt;')
@@ -99,6 +87,53 @@ class SMTPHandler(SMTPHandler):
             output.append(prettyformat(struct, indent))
         output.append('</div>')
         return '\n'.join(output)
+
+
+class SMTPHandler(SMTPHandler):
+
+    def __init__(self, *args, **kwargs):
+        super(SMTPHandler, self).__init__(*args, **kwargs)
+        self.formatter = self.formatter or HtmlFormatter()
+
+    def emit(self, record):
+        try:
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = self.getSubject(record)
+            msg['From'] = self.fromaddr
+            msg['To'] = ",".join(self.toaddrs)
+            msg['Date'] = formatdate()
+
+            text = self.format(record)
+            msg.attach(MIMEText(text.encode(sys.getfilesystemencoding()), 'plain'))
+            if record.exc_info:
+                html = self.format(record, html=True)
+                html = '<html><head></head><body>%s</body></html>' % html
+                msg.attach(MIMEText(html.encode(sys.getfilesystemencoding()), 'html'))
+            port = self.mailport
+            if not port:
+                port = smtplib.SMTP_PORT
+            smtp = smtplib.SMTP(self.mailhost, port)
+            if self.username:
+                if self.secure is not None:
+                    smtp.ehlo()
+                    smtp.starttls(*self.secure)
+                    smtp.ehlo()
+                smtp.login(self.username, self.password)
+            smtp.sendmail(self.fromaddr, self.toaddrs, msg.as_string())
+            smtp.quit()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
+    def format(self, record, html=False):
+        if self.formatter and isinstance(self.formatter, HtmlFormatter):
+            return self.formatter.format(record, html)
+        if self.formatter:
+            fmt = self.formatter
+        else:
+            fmt = _defaultFormatter
+        return fmt.format(record)
 
 
 class TermFormatter(Formatter):
